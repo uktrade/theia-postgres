252381476f82dc10761d2c381853c4a323553c14
Michal Charemza <michal@charemza.name>
2020-07-03 15:31:05 +0100

    (feat) Save new sql files in hard-coded home directory

--- src/commands/newQuery.ts
+++ src/commands/newQuery.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import BaseCommand from "../common/baseCommand";
 import * as vscode from 'vscode';
 import { IConnection } from "../common/IConnection";
@@ -7,8 +8,16 @@ import { EditorState } from "../common/editorState";
 
 export class newQueryCommand extends BaseCommand {
   async run(treeNode: any) {
-    // should have a connection object on it
-    const textDocument = await vscode.workspace.openTextDocument({content: '', language: 'postgres'});
+    // Probably a bit of a race condition if multiple calls at the same time
+    // but the user would have to be very quick
+    var index = 1;
+    const path = () => `/home/theia/untitled-${index}.sql`
+    while (fs.existsSync(path())) {
+      ++index;
+    }
+
+    const location = vscode.Uri.parse('untitled:' + path());
+    const textDocument = await vscode.workspace.openTextDocument(location);
     await vscode.window.showTextDocument(textDocument);
     if (treeNode && treeNode.connection)
       EditorState.connection = treeNode.connection;

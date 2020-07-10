49bb92f6febd0aa8dfb9a365487902efe5e1adf8
Michal Charemza <michal@charemza.name>
2020-07-10 22:59:10 +0100

    (refactor) Less depedency on vscode

--- src/commands/selectTop.ts
+++ src/commands/selectTop.ts
@@ -1,18 +1,18 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import * as vscode from 'vscode';
+import * as theia from '@theia/plugin';
 import { TableNode } from "../tree/tableNode";
 import { Client } from 'pg';
 
 
 export function getSelectTopCommand(runQueryAndDisplayResults) {
   return async function run(treeNode: TableNode) {
-    const countInput: string = await vscode.window.showInputBox({ prompt: "Select how many?", placeHolder: "limit" });
+    const countInput: string = await theia.window.showInputBox({ prompt: "Select how many?", placeHolder: "limit" });
     if (!countInput) return;
 
     const count: number = parseInt(countInput);
     if (Number.isNaN(count)) {
-      vscode.window.showErrorMessage('Invalid quantity for selection - should be a number');
+      theia.window.showErrorMessage('Invalid quantity for selection - should be a number');
       return;
     }
 
@@ -28,8 +28,8 @@ export function getSelectTopCommand(runQueryAndDisplayResults) {
     }
     fs.writeFileSync(getPath(), sql, 'utf8');
 
-    const textDocument = await vscode.workspace.openTextDocument(getPath());
-    await vscode.window.showTextDocument(textDocument);
+    const textDocument = await theia.workspace.openTextDocument(getPath());
+    await theia.window.showTextDocument(textDocument);
 
     const title = path.basename(textDocument.fileName);
     return runQueryAndDisplayResults(sql, treeNode.pool, textDocument.uri, title);

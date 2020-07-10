6d3aa8bf39a0023c7a199fea627fd6b87b1e2f3a
Michal Charemza <michal@charemza.name>
2020-07-10 22:51:32 +0100

    (refactor) Less dependancy on vscode

--- src/language/client.ts
+++ src/language/client.ts
@@ -1,10 +1,10 @@
 import * as path from 'path';
-import * as vscode from 'vscode';
+import * as theia from '@theia/plugin';
 import { LanguageClient, ServerOptions, TransportKind, LanguageClientOptions } from 'vscode-languageclient';
 import { ExtensionContext } from 'vscode';
 import { IConnectionConfig } from '../common/IConnectionConfig';
 
-export async function setupPostgresLanguageClient(context: ExtensionContext, connectionConfig: IConnectionConfig) {
+export async function setupPostgresLanguageClient(context: theia.PluginContext, connectionConfig: IConnectionConfig) {
   const serverModule = context.asAbsolutePath(path.join('lib', 'language', 'server.js'));
   const debugOptions = { execArgv: ['--nolazy', '--debug=6005', '--inspect'] };
 

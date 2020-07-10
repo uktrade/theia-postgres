32f70f94f18175930112dbe6952b0d0f58a722fe
Michal Charemza <michal@charemza.name>
2020-07-10 22:57:35 +0100

    (refactor) Less depedency on vscode

--- src/commands/runQuery.ts
+++ src/commands/runQuery.ts
@@ -1,18 +1,18 @@
 import * as path from 'path';
-import * as vscode from 'vscode';
+import * as theia from '@theia/plugin';
 import { Pool } from 'pg';
 
-export function getRunCommand(pool: Pool, runQueryAndDisplayResults: (sql: string, pool: Pool, uri: vscode.Uri, title: string) => void) {
+export function getRunCommand(pool: Pool, runQueryAndDisplayResults: (sql: string, pool: Pool, uri: theia.Uri, title: string) => void) {
   return async function run() {
-    if (!vscode.window.activeTextEditor || !vscode.window.activeTextEditor.document) {
-      vscode.window.showWarningMessage('No SQL file selected');
+    if (!theia.window.activeTextEditor || !theia.window.activeTextEditor.document) {
+      theia.window.showWarningMessage('No SQL file selected');
       return;
     }
 
-    const editor = vscode.window.activeTextEditor;
+    const editor = theia.window.activeTextEditor;
     const selectionToTrim = editor.selection.isEmpty ? undefined : editor.selection;
     if (editor.document.getText(selectionToTrim).trim().length === 0) {
-      vscode.window.showWarningMessage('No SQL found to run');
+      theia.window.showWarningMessage('No SQL found to run');
       return;
     }
 
